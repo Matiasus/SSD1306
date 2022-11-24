067b11ffab57603a29dfc5fc51299cea6241f5c7
matiasus <mato.hrinko@gmail.com>
2022-11-24 22:41:52 +0100

    Testing version 3.0

--- main.c
+++ main.c
@@ -31,32 +31,22 @@
  */
 int main(void)
 {
-  uint8_t addr = SSD1306_ADDRESS;
-
+/*
+  uint8_t x = 0;
+  uint8_t y = 0;
+*/
   // init ssd1306
-  SSD1306_Init (addr);
-
+  SSD1306_Init ();
   // clear screen
   SSD1306_ClearScreen ();
-  // draw line
-  SSD1306_DrawLine (0, MAX_X, 4, 4);
-  // set position
-  SSD1306_SetPosition (7, 1);
   // draw string
-  SSD1306_DrawString ("SSD1306 OLED DRIVER");
-  // draw line
-  SSD1306_DrawLine (0, MAX_X, 18, 18);
-  // set position
-  SSD1306_SetPosition (40, 3);
-  // draw string
-  SSD1306_DrawString ("MATIASUS");
-  // set position
-  SSD1306_SetPosition (53, 5);
-  // draw string
-  SSD1306_DrawString ("2021");
-  // update
-  SSD1306_UpdateScreen (addr);
+  SSD1306_DrawString ("SSD1306 MATIASUS");
+/*
+  while (x < 10) {
 
+    SSD1306_DrawPixel (x++,y++);
+  }
+*/
   // return value
   return 0;
 }
