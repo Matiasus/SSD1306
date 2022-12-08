a6739f6a08444545ce7ab5d49354bac238cdf787
matiasus <mato.hrinko@gmail.com>
2022-12-08 22:37:11 +0100

    Testing new functions

--- main.c
+++ main.c
@@ -6,13 +6,13 @@
  *              Written by Marian Hrinko (mato.hrinko@gmail.com)
  *
  * @author      Marian Hrinko
- * @datum       06.10.2020
+ * @date        06.10.2020
  * @update      19.07.2021
  * @file        main.c
  * @version     3.0
  * @tested      AVR Atmega328p
  *
- * @depend      ssd1306.h
+ * @depend      lib/ssd1306.h
  * --------------------------------------------------------------------------------------+ 
  * @descr       Version 1.0 -> applicable for 1 display
  *              Version 2.0 -> rebuild to 'cacheMemLcd' array
@@ -22,7 +22,6 @@
 
 // include libraries
 #include "lib/ssd1306.h"
-#include <util/delay.h>
 
 /**
  * @desc    Main function
@@ -34,35 +33,22 @@
 int main(void)
 {
   uint8_t i = 0;
-  // init ssd1306
+  
   SSD1306_Init ();
-  // clear screen
   SSD1306_ClearScreen ();
-/*
-  while (x < MAX_Y) {
-    SSD1306_DrawLine (x,x,0,y++);
-    x++;
-  }
-*/
-  //SSD1306_DrawLine (0,MAX_X,1,1);
-  //SSD1306_DrawLine (5,5,0,7);
-  //SSD1306_DrawLine (7,7,2,7);
-  //SSD1306_DrawLine (0,MAX_X,20,20);
-  //SSD1306_DrawLine (9,9,0,9);
-  //SSD1306_DrawLine (0,MAX_X,20,20);
-
-  //SSD1306_DrawLineHorz (1,0,MAX_X);
-  //SSD1306_DrawLineHorz (10,0,MAX_X);
 
   while (i < MAX_Y) {
-    SSD1306_DrawLine (0,MAX_X-10,i,i);
-    //_delay_ms (100);
+    SSD1306_DrawLine (0,MAX_X,i,i);
     i++;
   }
 
-  SSD1306_SetPosition (0, 0) ;
-  // draw string
-  SSD1306_DrawString ("SSD1306 MATIASUS Copyright (C) 2020 Marian Hrinko. Copyright (C) 2020 Marian Hrinko. Copyright (C) 2020 Marian Hrinko.");
+  SSD1306_SetPosition (25, 0) ;
+  SSD1306_DrawString ("SSD1306", BOLD);
+
+  SSD1306_SetPosition (4, 2) ;
+  SSD1306_DrawString ("by ", NORMAL);
+  SSD1306_DrawString ("MATIASUS", NORMAL | UNDERLINE);
+  SSD1306_DrawString (" (C) 2022", NORMAL);
 
   // return value
   return 0;
